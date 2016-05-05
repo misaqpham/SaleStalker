2abefbb8309cd0ad83c4ddaf2abfb10de60f8a3f
Misa Pham <mpham3@lion.lmu.edu>
2016-05-04 18:04:07 -0700

    Stuff

--- SaleStalker/BrowseTableViewController.swift
+++ SaleStalker/BrowseTableViewController.swift
@@ -14,9 +14,7 @@ import UIKit
 class BrowseTableViewController: UITableViewController {
     
     //var webView: WKWebView
-    //var sectionTitles = ["Leave Feedback", "Follow Us"]
-    var sectionContent = [["Revolve Clothing"],
-                          ["Twitter", "Facebook", "Pinterest"]]
+    var sectionContent = [["Revolve Clothing"]]
     var links = ["https://revolve.com"]
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -35,11 +33,6 @@ class BrowseTableViewController: UITableViewController {
         tableView.tableFooterView = UIView(frame: CGRectZero)
     }
     
-//    override func tableView(tableView: UITableView, titleForHeaderInSection
-//        section: Int) -> String? {
-//        return sectionTitles[section]
-//    }
-
     override func tableView(tableView: UITableView, cellForRowAtIndexPath
         indexPath: NSIndexPath) -> UITableViewCell {
         let cellIdentifier = "Cell"
@@ -60,32 +53,7 @@ class BrowseTableViewController: UITableViewController {
         }
 
     }
-    
-    
-    
-//    required init?(coder aDecoder: NSCoder) {
-//        self.webView = WKWebView(frame: CGRectZero)
-//        super.init(coder: aDecoder)
-//        
-//        self.webView.navigationDelegate = self
-//    }
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        // Do any additional setup after loading the view, typically from a nib.
-//
-//        view.addSubview(webView)
-//        webView.translatesAutoresizingMaskIntoConstraints = false
-//        let height = NSLayoutConstraint(item: webView, attribute: .Height, relatedBy: .Equal, toItem: view, attribute: .Height, multiplier: 1, constant: 0)
-//        let width = NSLayoutConstraint(item: webView, attribute: .Width, relatedBy: .Equal, toItem: view, attribute: .Width, multiplier: 1, constant: 0)
-//        view.addConstraints([height, width])
-//        
-//        let url = NSURL(string:"http://www.google.com")
-//        let request = NSURLRequest(URL:url!)
-//        webView.loadRequest(request)
-//        
-//    }
-//    
+      
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- SaleStalker/WishListTableViewController.swift
+++ SaleStalker/WishListTableViewController.swift
@@ -27,7 +27,7 @@ class WishListTableViewController: UITableViewController {
         return 1
     }
     
-   /* override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return 0
     }
@@ -37,24 +37,7 @@ class WishListTableViewController: UITableViewController {
         
         return tableView.dequeueReusableCellWithIdentifier("ItemCellTableViewCell") as! ItemCellTableViewCell
         
-    }*/
-    
-    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional","Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
-    
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) ->
-        Int {
-        // Return the number of rows in the section.
-        return restaurantNames.count
     }
     
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath:
-            NSIndexPath) -> UITableViewCell {
-            let cellIdentifier = "Cell"
-            let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier,
-            forIndexPath: indexPath)
-            // Configure the cell...
-            cell.textLabel?.text = restaurantNames[indexPath.row]
-            return cell }
 
-    
 }
